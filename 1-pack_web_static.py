1879dfe59fc023ee31d54036d22e16607ac2a880
alantiren <tirenalan0@gmail.com>
2023-09-07 16:21:02 +0300

    tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,21 +10,15 @@ def do_pack():
     """
     Generates a .tgz archive from the web_static folder
     """
-
     if not os.path.exists("versions"):
         os.makedirs("versions")
-
     archive_name = "web_static_{}.tgz".format(
         now.strftime("%Y%m%d%H%M%S"))
-
     archive_command = "tar -cvzf versions/{} web_static".format(archive_name)
-
     result = local(archive_command)
-
     if result.succeeded:
         return "versions/{}".format(archive_name)
     else:
         return None
-
 if __name__ == "__main__":
     do_pack()
