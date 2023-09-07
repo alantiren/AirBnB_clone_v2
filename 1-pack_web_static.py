f080f1cd52fa9d6ac162c41ff123d00d94042bd2
alantiren <tirenalan0@gmail.com>
2023-09-07 16:14:54 +0300

    tasks

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-Fabric script that generates a .tgz archive from the contents of the
-web_static folder of your AirBnB Clone repo
-"""
+# Fabric script that generates a .tgz archive from the contents of the
+# web_static folder of your AirBnB Clone repo
 from fabric.api import local
 from datetime import datetime
 import os
