89669c90227775c9df4ccd7c6a59804722db08d1
alantiren <tirenalan0@gmail.com>
2023-09-07 14:01:01 +0300

    tasks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,32 +2,46 @@
 # Script to prepare web servers for web_static deployment
 
 # Install Nginx if it's not already installed
-if ! [ -x "$(command -v nginx)" ]; then
-    apt-get update
-    apt-get -y install nginx
-fi
+apt-get update
+apt-get install -y nginx
 
 # Create necessary directories if they don't exist
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
 # Create a fake HTML file for testing
-echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 
 # Create or recreate a symbolic link
-rm -f /data/web_static/current
-ln -s /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership to the ubuntu user and group recursively
-chown -R ubuntu:ubuntu /data/
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
 # Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-if grep -q 'location /hbnb_static {' "$config_file"; then
-    sed -i '/location \/hbnb_static {/!b;n;c alias /data/web_static/current/;' "$config_file"
-else
-    sed -i '/server_name _;/a location /hbnb_static {\n alias /data/web_static/current/;\n}' "$config_file"
-fi
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
 # Restart Nginx
 service nginx restart
